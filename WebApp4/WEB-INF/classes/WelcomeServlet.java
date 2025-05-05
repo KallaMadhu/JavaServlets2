f56bec6b19ba87dc1e38359e633ec8ec124cd625
Madhu.K <kallamadhu10@gmail.com>
2025-05-05 14:57:56 +0530

    comment 1.1

---
+++ WebApp4/WEB-INF/classes/WelcomeServlet.java
@@ -0,0 +1,15 @@
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+public class WelcomeServlet extends HttpServlet{
+
+    public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
+    {   
+        res.setContentType("text/html");
+        PrintWriter out=res.getWriter();
+        String uname=req.getParameter("uname");
+        //uname=request.GET['uname']
+        out.println("Welcome..."+uname);
+        out.close();
+    }
+}
